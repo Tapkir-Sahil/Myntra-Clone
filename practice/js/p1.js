ccd4d34bda554e6fe5999bb97e8c0bb247f401b5
sahil-tapkir <sahiltapkir07@gmail.com>
2025-01-23 12:13:24 +0530

    Add p1.js file with console log statements for bill calculations and GST

---
+++ practice/js/p1.js
@@ -0,0 +1,10 @@
+console.log(`For Bill Amount:-`);
+console.log(`Final Bill Amount RS:${(1*100)+(2*10)+(1*30)}`);
+console.log(`For 18% GST:-`);
+console.log(`TOTAL PRICE WITH GST:${((1*79990)+(2*24990))+180/100*129970}`);
+console.log(`GST 18%:${363916-129970}`);
+console.log(`GST 18%:${Math.round((1*79990)+(2*24990))*18/100}`);
+
+console.log('HELLO '+' WORLD');
+console.log("Hii "+1);
+console.log(`Hello ${10+9}`);
