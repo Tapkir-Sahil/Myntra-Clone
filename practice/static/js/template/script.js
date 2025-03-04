ca8496a1849c255aa9f3ab6bd9e901a053df72ff
sahil-tapkir <sahiltapkir07@gmail.com>
2025-03-04 12:32:44 +0530

    Add initial HTML, CSS, and JavaScript files for practice template: create structure, styles, and button functionality

---
+++ practice/static/js/template/script.js
@@ -0,0 +1,13 @@
+alert('WELCOME TO THIS PAGE')
+
+// document.querySelector('#btn1').alert('ADD TO BAG')
+// document.querySelector('#btn2').alert('ADD TO WISHLIST')
+
+
+document.querySelector('#btn1').addEventListener('click',function(){
+  alert('ADD TO BAG');
+});
+
+document.querySelector('#btn2').addEventListener('click',function(){
+  alert('ADD TO WIShLIST');
+});
