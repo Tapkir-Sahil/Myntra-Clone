ca8aa0bce7da94e7a933d765972b190a51c50e9f
sahil-tapkir <sahiltapkir07@gmail.com>
2025-03-07 18:55:04 +0530

    Add initial HTML, CSS, and JavaScript files for chapter 3: create structure for Rock Paper Scissors game

---
+++ practice/chapter 3/practice.js
@@ -0,0 +1,9 @@
+// let age = 5;
+// if (age>=60) {
+//   console.log('You Can Drive But probably Ask Someone To Drive')
+// } else if(age>=18){
+//   console.log('You Can Drive')
+// }else{
+//   console.log('You Cannot Drive')
+// }
+
