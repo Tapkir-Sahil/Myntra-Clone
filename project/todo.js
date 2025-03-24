db6fb3f17fd4c42da496a5830c3c8df64441f4b7
sahil-tapkir <sahiltapkir07@gmail.com>
2025-03-24 19:03:40 +0530

    Add delete button functionality and enhance todo list display; update styles and HTML structure

--- project/todo.js
+++ project/todo.js
@@ -1,22 +1,36 @@
 let todoList = [
   {
-    item : 'JS',
-    dueDate : '22/03/2025'
+    item: 'JS',
+    dueDate: '22/03/2025'
   },
   {
-    item : 'CodeAndChai-JS',
-    dueDate : '25/03/2025'
+    item: 'Code-And-Chai-JS',
+    dueDate: '25/03/2025'
   },
 ];
+todoDisplay();
 
-function addClick(){
+function addClick() {
   let inputElement = document.querySelector('#todo-input');
   let inputDate = document.querySelector('#todo-date')
   let todoItem = inputElement.value;
   let todoDate = inputDate.value;
-  todoList.push({item: todoItem, dueDate: todoDate });
+  todoList.push({ item: todoItem, dueDate: todoDate });
   console.log(todoList)
   inputElement.value = '';
   inputDate.value = '';
+  todoDisplay();
 }
 
+function todoDisplay() {
+  let containerElement = document.querySelector('.todo-container');
+  let newHtml = '';
+  for (let i = 0; i < todoList.length; i++) {
+    let { item, dueDate } = todoList[i];
+    newHtml += `<span>${item}</span>
+                  <span>${dueDate}</span>
+                  <button id="btn-delete" onclick="todoList.splice(${i},1);
+                  todoDisplay(); ">Delete</button>`;
+  }
+  containerElement.innerHTML = newHtml;
+}
