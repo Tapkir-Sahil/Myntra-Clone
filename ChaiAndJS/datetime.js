38a8df4952901d917bd16e1ee874804e7da6280b
sahil-tapkir <sahiltapkir07@gmail.com>
2025-04-16 17:36:18 +0530

    Add datetime.js for demonstrating various Date methods in JavaScript

---
+++ ChaiAndJS/datetime.js
@@ -0,0 +1,10 @@
+
+// Dates Method in JS
+let mydate = new Date();
+console.log(mydate.toDateString());
+console.log(mydate.toJSON());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString())
+console.log(mydate.toLocaleDateString())
+let mydateday = new Date(2003,0,23,9,5);
+console.log(mydateday.toLocaleString());
