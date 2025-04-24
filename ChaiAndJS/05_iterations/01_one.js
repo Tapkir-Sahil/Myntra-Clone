3f803e3cd0f81c4334639f93429274d695621230
sahil-tapkir <sahiltapkir07@gmail.com>
2025-04-24 12:06:48 +0530

    Refactor: Comment out unused for loop and enhance table generation example

--- ChaiAndJS/05_iterations/01_one.js
+++ ChaiAndJS/05_iterations/01_one.js
@@ -1,10 +1,20 @@
 //for loop
 
-for (let i = 0; i <= 10; i++) {
-  const element = i;
-  if( i == 5){
-    console.log("5 Is Best")
+// for (let i = 0; i <= 10; i++) {
+//   const element = i;
+//   if( i == 5){
+//     console.log("5 Is Best")
+//   }
+//   console.log(element)
+// }
+
+// For checking inner loop and outer loop
+
+for (let i = 1; i <= 10; i++){
+  // console.log(`this is outer loop ${i}`)
+  console.log(`this is Table ${i}`)
+  for(let j = 1; j <=10 ; j++){
+    console.log(`${i}*${j} = ${j*i}`)
   }
-  console.log(element)
 }
 
